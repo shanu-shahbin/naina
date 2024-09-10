fed6609ae6819318f2f99d6d5998f5805f7c2e66
SHANU SHAHBIN <shanushahbin11@gmail.com>
2024-09-10 19:40:26 +0530

    Update urls.py

--- GoldenNaina/urls.py
+++ GoldenNaina/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
+    path('', include('products.urls')),
     path('customers/', include('Customers.urls')),
     path('orders/', include('orders.urls')),
     path('home/', include('HomeApp.urls')),
